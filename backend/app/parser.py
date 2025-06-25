ce1544afbe71dc13e0cf6097ddbbded126cd5e79
vyshnavi-kunapareddy <127083979+vyshnavi-kunapareddy@users.noreply.github.com>
2025-06-25 20:41:29 +0530

    adding requirements

--- backend/app/parser.py
+++ backend/app/parser.py
@@ -7,7 +7,12 @@ from transformers import pipeline
 from app.utils.layout_section_extractor import extract_sections_as_json
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 
 ner_pipeline = pipeline(
